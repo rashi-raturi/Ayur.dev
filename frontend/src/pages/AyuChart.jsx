09d9349800cd5becfa1dd2b297a0aecd9fe27b60
stvy <stvycoding@gmail.com>
2025-09-19 02:22:36 +0530

    Add : diet chart generator frontend

--- frontend/src/pages/AyuChart.jsx
+++ frontend/src/pages/AyuChart.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import DietChartGenerator from '../components/DietChartGenerator';
 
 const AyuChart = () => {
   return (
-    <div>
-      <h1>AyuChart</h1>
-    </div>
+    <DietChartGenerator/>
   )
 }
 
